85f51f55a2b24195bd46e0f2e42bf3ec24414bff
Ryan <yarnnd@gmail.com>
2023-01-19 13:29:28 -0800

    tilt: fix data collection template

--- tilt_hardware_control/tilt_docs/data_collection_template/run_bias.sh
+++ tilt_hardware_control/tilt_docs/data_collection_template/run_bias.sh
@@ -1,6 +1,6 @@
 
 source "$HOME/Documents/tilt_hardware_control/setup_data_collection.sh"
 
-tilt --config config.hjson --bias "tilt_output/bias_$(dt).csv"
+tilt --config config.hjson --labels labels.hjson --bias "tilt_output/bias_$(dt).csv"
 
 post
--- tilt_hardware_control/tilt_docs/data_collection_template/run_monitor.sh
+++ tilt_hardware_control/tilt_docs/data_collection_template/run_monitor.sh
@@ -1,6 +1,6 @@
 
 source "$HOME/Documents/tilt_hardware_control/setup_data_collection.sh"
 
-tilt --config config.hjson --monitor --live --live-secs 30 --loadcell-out "tilt_output/notilt_$(dt).csv"
+tilt --config config.hjson --labels labels.hjson --monitor --live --live-secs 30 --loadcell-out "tilt_output/notilt_$(dt).csv"
 
 post
